e4939b6f0cfb1f7fd63c4f7370a5153c673b98c1
joohuil <99111909+joohuil@users.noreply.github.com>
2024-12-11 16:18:40 +1300

    separated middleware

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,15 @@
-const express = require('express')
+import express from 'express'
 const app = express()
-const cors = require('cors')
-require('dotenv').config()
+import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const Event = require('./models/event')
-const Account = require('./models/account')
+import Event from './models/event.js'
+import Account from './models/account.js'
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
-
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-      return response.status(400).json({ error: error.message })
-    }
-
-    next(error)
-}
+import requestLogger from './middleware/requestLogger.js'
+import unknownEndpoint from './middleware/unknownEndpoint.js'
+import errorHandler from './middleware/errorHandler.js'
 
 app.use(cors())
 app.use(express.json())
